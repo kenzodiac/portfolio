dfa3ccb5be1eaac659911b0cb2f691b60d5404d7
kenzodiac <kennethfujimura@gmail.com>
2023-05-23 15:03:55 -0700

    fixed some mobile responsiveness issues

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -14,7 +14,7 @@ export default function Homepage() {
             <Col lg={4} md={5}>
                 <AboutMe/>
             </Col>
-            <Col lg={8} md={7} className='overflow-auto' style={{height: '90vh'}}>
+            <Col lg={8} md={7} className='overflow-rules'>
                 <Landing/>
                 <Skills/>
                 <Projects/>
