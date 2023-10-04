27879b3a6ff569e9fc530b4d6367b76fb7e08a91
kenzodiac <kennethfujimura@gmail.com>
2023-10-03 23:50:53 -0700

    small edit to hours in landing description

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -13,7 +13,7 @@ export default function Landing() {
                 <span style={{ fontSize: 'calc(10px + 4vmin)' }}>{text}</span>
             </div>
             <div className={"mt-2 landing-about-txt-div"}>
-                <span className={"landing-subtitle"}>I've been an educator for over a decade, but now I'm challenging myself to take a leap into an exiciting career change of becoming a web developer. To date, I've spent nine months and over 1,000 hours of intense, instructor-led coursework to learn a firm foundation in web development. Combining all the skills I've accured through working as a teacher, the experience and maturity I've gained over the years, and this new passion for coding, I hope to make my mark and leave a lasting impression on everyone and everything I work with.</span>
+                <span className={"landing-subtitle"}>I've been an educator for over a decade, but now I'm challenging myself to take a leap into an exiciting career change of becoming a web developer. To date, I've spent nine months and over 1,200 hours of intense, instructor-led coursework to learn a firm foundation in web development. Combining all the skills I've accured through working as a teacher, the experience and maturity I've gained over the years, and this new passion for coding, I hope to make my mark and leave a lasting impression on everyone and everything I work with.</span>
             </div>
         </div>
     )
